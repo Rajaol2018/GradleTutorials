4619b700f2e5278da3655f14f6ffc05946c5b7c9
rajeesharajesh <rajaol2018@gmail.com>
2025-04-24 14:51:47 +0530

    The first file from learn gradle

---
+++ src/main/java/org/example/Main1.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number between 1 and 5:");
+        scanner.nextLine();
+        System.out.println("Hello, World!"+scanner.nextLine());
+    }
+}
