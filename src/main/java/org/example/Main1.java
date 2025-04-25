dbf0c3bf2ed2afd1471bc60233a9985e51be8159
rajeesharajesh <rajaol2018@gmail.com>
2025-04-25 16:42:59 +0530

    message simply

--- src/main/java/org/example/Main1.java
+++ src/main/java/org/example/Main1.java
@@ -5,12 +5,7 @@ import java.util.Scanner;
 public class Main1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a number between 1 and 5:");
-        scanner.nextLine();
-        System.out.println("Hello, World!"+scanner.nextLine());
-        System.out.println("Next line");
-        scanner.nextLine();
         System.out.println("Training local");
-
+        scanner.nextLine();
     }
 }
